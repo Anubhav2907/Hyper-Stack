ac2f9e41b56c699681110faed722cda441b49586
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-04-09 16:51:07 +0530

    staff animations

--- public/app.js
+++ public/app.js
@@ -1,30 +1,6 @@
 AOS.init({
-  offset: 400, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000 // values from 0 to 3000, with step 50ms
+    offset: 400, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000,
+    easing: 'ease-in-out-back' // values from 0 to 3000, with step 50ms
 });
-document.addEventListener('DOMContentLoaded',function(event){
-    var dataText = ["DEPARTMENT OF ELECTRONICS AND COMMUNICATION ENGINEERING"];
-    function typeWriter(text, i, fnCallback) {
-      if (i < (text.length)) {
-       document.querySelector(".dept").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-        setTimeout(function() {
-          typeWriter(text, i + 1, fnCallback)
-        }, 70);
-      }
-    }
-
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 20000);
-       }
-      if (i < dataText[i].length) {
-       typeWriter(dataText[i], 0, function(){
-         StartTextAnimation(i + 1);
-       });
-      }
-    }
-    StartTextAnimation(0);
-  });
